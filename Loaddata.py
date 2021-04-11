fc8c4d4aae1bef4c30e9ee4da07f9f41f0c1ec94
THONG <THONG@LAPTOP-DJ9J9KHA>
2021-04-11 22:00:40 +0700

    còn phần rút trích đặc trưng và báo cáo

--- Loaddata.py
+++ Loaddata.py
@@ -1,5 +1,7 @@
+import itertools
 import math
-
+from sklearn.metrics import mean_squared_error
+import math
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
@@ -67,15 +69,15 @@ print("Giá trị trung bình: ".ljust(39) + str(list_Urate[1]))
 print("Giá trị trung vị: ".ljust(40) + str(list_Urate[2]))
 print("Giá trị lớn nhất: ".ljust(39) + str(list_Urate[3]))
 
+df_cor= pd.DataFrame(data=clean_data, columns=['GDP', "Unemployment rate"])
+
 print("\n--------------Độ tương quan---------------\n")
-print(clean_data.corr().round(2))
+print(df_cor.corr().round(2))
 
 
 # biểu đồ thể hiện độ tương quan
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-df_cor= pd.DataFrame(data=clean_data, columns=['GDP', "Unemployment rate"])
 
 plt.figure(figsize = (14,8))
 colormap = sns.diverging_palette(145, 10, as_cmap=True)
@@ -95,34 +97,61 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 lin_reg=LinearRegression()
 lin_reg.fit(X_train,y_train)
-y_pred = lin_reg.predict(X_test)
-print(y_pred)
-from sklearn.metrics import accuracy_score
+y_pred1 = lin_reg.predict(X_test)
+# print(y_pred1)
+linear_mse = mean_squared_error(y_test, y_pred1)
+linear_rmse = math.sqrt(mean_squared_error(y_test, y_pred1))
 
 #Visualisng the linear regression model results
 plt.scatter(X,y,color='red')
 plt.plot(X,lin_reg.predict(X),color='blue')
-plt.title('Truth or bluff(Linear Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+plt.title('Linear Regression')
+plt.xlabel('GDP')
+plt.ylabel('Unemployment rate')
 plt.show()
 
-
-
-
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg=PolynomialFeatures(degree=4)
+poly_reg=PolynomialFeatures(degree=2)
 X_poly=poly_reg.fit_transform(X)
 poly_reg.fit(X_poly,y)
 lin_reg2=LinearRegression()
 lin_reg2.fit(X_poly,y)
-
+y_pred2 = lin_reg2.predict(poly_reg.fit_transform(np.array(X_test)))
+# print(y_pred)
+poly_mse = mean_squared_error(y_test, y_pred2)
+poly_rmse = math.sqrt(mean_squared_error(y_test, y_pred2))
 #Visualising the pollynomial regression model results
 X_grid=np.arange(min(X),max(X),0.1)
 X_grid=X_grid.reshape((len(X_grid),1))
 plt.scatter(X,y,color='red')
 plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)),color='blue')
-plt.title('Truth or bluff(Polynomial Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+plt.title('Polynomial Regression')
+plt.xlabel('GDP')
+plt.ylabel('Unemployment rate')
 plt.show()
+
+print("\n------SO SÁNH MÔ HÌNH HỒI QUY-------------\n")
+print("\n-------------Linear Regression------------\n")
+print("Sai số toàn phương trung bình: ", round(linear_mse,2))
+print("Độ lệch chuẩn sai số: ", round(linear_rmse,2))
+X_test = X_test.tolist()
+X_test = sum(X_test, [])
+print("\nKết quả dự đoán")
+print("GPD:".ljust(25), end="")
+for i in X_test:
+    print(str(round(i,2)).ljust(10), end=" ")
+print("\nUnemplement rate:".ljust(25), end=" ")
+for i in y_pred1:
+    print(str(round(i,2)).ljust(10), end=" ")
+
+
+print("\n\n----------Polinomial Regression-----------\n")
+print("Sai số toàn phương trung bình: ", round(poly_mse, 2))
+print("Độ lệch chuẩn sai số: ", round(poly_rmse,2))
+print("\nKết quả dự đoán")
+print("GPD:".ljust(25), end="")
+for i in X_test:
+    print(str(round(i,2)).ljust(10), end=" ")
+print("\nUnemplement rate:".ljust(25), end=" ")
+for i in y_pred2:
+    print(str(round(i,2)).ljust(10), end=" ")
